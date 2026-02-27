5332996c782e4e4b763d3d5e58414af1c15fb173
Roger Agostinho <167251142+rogeragostinho@users.noreply.github.com>
2026-02-27 21:30:39 +0100

    feat: configurações iniciais

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/data/latest_all.dart';
+import 'package:timezone/timezone.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,10 +14,55 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: MyHomePage(),
       ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final FlutterLocalNotificationsPlugin notificationsPlugin = 
+    FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  Future<void> init() async {
+    initializeTimeZones();
+
+    //! https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+    setLocalLocation(getLocation('Africa/Luanda'));
+
+    const androidSettings = 
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+    const DarwinInitializationSettings iosSettings = 
+      DarwinInitializationSettings();
+
+    const InitializationSettings initializationSettings = 
+      InitializationSettings(
+        android: androidSettings,
+        iOS: iosSettings,
+      );
+      await notificationsPlugin.initialize(settings: initializationSettings);
+  }
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("ola")
     );
   }
 }
